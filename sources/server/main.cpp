aaa1cb1970b7626371e1f0f83e37d4c957c090f6
AsianPw <brice.lang-nguyen@epitech.eu>
2018-09-17 12:13:02 +0200

    Add all package in conan

--- sources/server/main.cpp
+++ sources/server/main.cpp
@@ -5,7 +5,29 @@
 ** Created by asianpw,
 */
 
+#include <iostream>
+#include <boost/chrono.hpp>
+
+using namespace boost::chrono;
+
 int	main(int ac, char *av[])
 {
+	std::cout << system_clock::now() << std::endl;
+	#ifdef BOOST_CHRONO_HAS_CLOCK_STEADY
+	std::cout << steady_clock::now() << std::endl;
+	#endif
+	std::cout << high_resolution_clock::now() << std::endl;
+
+	#ifdef BOOST_CHRONO_HAS_PROCESS_CLOCKS
+	std::cout << process_real_cpu_clock::now() << std::endl;
+	std::cout << process_user_cpu_clock::now() << std::endl;
+	std::cout << process_system_cpu_clock::now() << std::endl;
+	std::cout << process_cpu_clock::now() << std::endl;
+	#endif
+
+	#ifdef BOOST_CHRONO_HAS_THREAD_CLOCK
+	std::cout << thread_clock::now() << std::endl;
+	#endif
+
 	return 0;
 }
