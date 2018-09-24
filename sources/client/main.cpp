d4a713084211a58fa3eb58284142e894a2b22ba2
brice lang-nguyen <brice.lang-nguyen@epitech.eu>
2018-09-24 16:32:59 +0200

    Fix QT installation

--- sources/client/main.cpp
+++ sources/client/main.cpp
@@ -8,16 +8,16 @@
 #include <portaudio.h>
 #include <iostream>
 #include <memory>
+#include <QApplication>
+#include <QPushButton>
 #include <IAudio.hpp>
 #include <PortAudio.hpp>
 
 int	main(int ac, char *av[])
 {
-	try {
-		std::unique_ptr<Babel::IAudio> audio(new Babel::PortAudio());
-	} catch (std::runtime_error const &error) {
-		std::cerr << error.what() << std::endl;
-		return 84;
-	}
-	return 0;
+	std::cout << "hdsqljdmlsqjofdjszpfjdhùpdkjosq" << std::endl;
+	QApplication app(ac, av);
+	QPushButton bouton("Salut les Zéros, la forme ?");
+	bouton.show();
+	return app.exec();
 }
