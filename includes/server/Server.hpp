2f3e392c573e7930df9e39fddadbcd2b94c3259a
AsianPw <brice.lang-nguyen@epitech.eu>
2018-10-07 02:29:04 +0200

    [Conan/Boost] All glitch are fix for now :/

--- includes/server/Server.hpp
+++ includes/server/Server.hpp
@@ -7,15 +7,6 @@
 #ifndef CPP_BABEL_2018_SERVER_HPP
 #define CPP_BABEL_2018_SERVER_HPP
 
-#ifdef	__unix__
-#include <sys/types.h>
-	#include <sys/socket.h>
-	#include <netinet/in.h>
-	#include <arpa/inet.h>
-#elif	_WIN32
-	#include <winsock2.h>
-#endif
-
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/chrono.hpp>
