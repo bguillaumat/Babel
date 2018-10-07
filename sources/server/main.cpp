618f0f7444120a8ff60d93bd4eaf8e7970e97732
AsianPw <brice.lang-nguyen@epitech.eu>
2018-10-07 02:05:34 +0200

    [Conan] Change boost/conan to boost/bincrafters

--- sources/server/main.cpp
+++ sources/server/main.cpp
@@ -7,29 +7,8 @@
 
 #include "Server.hpp"
 
-/*int	main(int ac, char *av[])
-{
-	std::cout << system_clock::now() << std::endl;
-	#ifdef BOOST_CHRONO_HAS_CLOCK_STEADY
-	std::cout << steady_clock::now() << std::endl;
-	#endif
-	std::cout << high_resolution_clock::now() << std::endl;
 
-	#ifdef BOOST_CHRONO_HAS_PROCESS_CLOCKS
-	std::cout << process_real_cpu_clock::now() << std::endl;
-	std::cout << process_user_cpu_clock::now() << std::endl;
-	std::cout << process_system_cpu_clock::now() << std::endl;
-	std::cout << process_cpu_clock::now() << std::endl;
-	#endif
-
-	#ifdef BOOST_CHRONO_HAS_THREAD_CLOCK
-	std::cout << thread_clock::now() << std::endl;
-	#endif
-
-	return 0;
-}*/
-
-int	main(void)
+int	main()
 {
 	try
 	{
