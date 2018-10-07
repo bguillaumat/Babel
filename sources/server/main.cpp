82c826df5beb47b8e45a576154f48756a6a5937e
j-f@nfj-hp-elitebook-840-g3 <johanne-franck.ngbokoli@epitech.eu>
2018-10-07 17:50:33 +0200

    main exception

--- sources/server/main.cpp
+++ sources/server/main.cpp
@@ -8,12 +8,24 @@
 #include "Server.hpp"
 
 
-int	main()
+int	main(int ac, char **argv)
 {
+	int	port;
+
+	if (ac != 2) {
+		std::cerr << "Usage: ./babel_server port" << std::endl;
+		std::cerr << "Port must me a number" << std::endl;
+		return (84);
+	}
+	port = atoi(argv[1]);
+	if (port <= 0) {
+		std::cerr << "Bad port value" << std::endl;
+		return (84);
+	}
 	try
 	{
 		boost::asio::io_service io_service;
-		Tcp server(io_service, 8080);
+		Tcp server(io_service, port);
 		io_service.run();
 	}
 	catch (std::exception& e)
